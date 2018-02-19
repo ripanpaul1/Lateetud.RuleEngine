c2d4bc64fc1aa2dde7fa435c2a9086c4d19f7a4b
prabanerjee01 <prasanta.banerjee@lateetud.com>
2018-02-19 12:31:22 +0530

    Service related changes

---
+++ BLL/RuleApplicationSummaryHandler.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RuleApplicationSummaryHandler
+    {
+        RuleApplicationSummaryDBAccess ruleSummaryDB = null;
+        public RuleApplicationSummaryHandler()
+        {
+            ruleSummaryDB = new RuleApplicationSummaryDBAccess();
+        }
+        public bool AddNew(RuleApplicationSummary objRuleSummary)
+        {
+            return ruleSummaryDB.AddNew(objRuleSummary);
+        }
+        public RuleApplicationSummary GetRunID()
+        {
+            return ruleSummaryDB.GetRunID();
+        }
+    }
+}
