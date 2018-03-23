40a84880b5449731fd297b6afcbffdf732bcfd35
Aindree <aindree.banerjee@lateetud.com>
2018-03-23 10:12:16 -0400

    initial commit for validation

---
+++ DAL/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Validation
+    {
+        public long RuleDescID { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ExpressionContext { get; set; }
+        public string ExpressionText { get; set; }
+
+    }
+}
+
